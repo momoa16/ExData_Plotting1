c9b19d98bd32951467694684c47eefd9f50f3674
momoa16 <mohamed.aounallah@gmail.com>
2015-09-11 20:16:48 +0300

    Removed some unnecessary comments

--- plot1.R
+++ plot1.R
@@ -7,9 +7,7 @@ unlink(temp)
 # Loading Data
 # Assuming that the file doesn't change. i.e. 1/2/2007 starts at line 66638 and
 #   2 days takes 2880 rows (60*24*2)
-# if the file is already dowloaded comment the following line and uncomment the line after
 colN <- names(read.csv("household_power_consumption.txt", sep = ";", nrows = 1))
-#colN <- names(read.csv("household_power_consumption.txt", sep = ";", nrows = 1))
 epc <- read.csv("household_power_consumption.txt", sep = ";", skip = 66636, nrows = 2880, header = TRUE, na.strings = "?", col.names = colN)
 #epc$Time <- as.POSIXct(paste(epc$Date, epc$Time), format="%d/%m/%Y %H:%M:%S")
 epc$Time <- strptime(paste(epc$Date, epc$Time), format="%d/%m/%Y %H:%M:%S")
